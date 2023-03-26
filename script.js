181f7b7babdf8d7505c8dea3a948f39e63aecb7f
Galactic Network <114846211+GalacticNetwork@users.noreply.github.com>
2023-03-26 17:24:14 +0000

    Added an accurate analytics

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 		function ab() {
-			var viewFrame; try {viewFrame = window !== top} catch (f) {viewFrame = !0} if (!viewFrame && !navigator.userAgent.includes("Firefox")) {let c = open("about:blank", "_blank"); if (!c || c.closed) alert("Allow popups and redirects to hide this from showing up in your history.\nThanks!\JMW"); else {let b = c.document, d = b.createElement("iframe"), a = d.style, e = b.createElement("link"); b.title = "Google Drive", e.rel = "icon", e.href = "https://ssl.gstatic.com/images/branding/product/1x/drive_2020q4_32dp.png", d.src = location.href, a.position = "fixed", a.top = a.bottom = a.left = a.right = 0, a.border = a.outline = "none", a.width = a.height = "100%", b.body.appendChild(d), location.replace("https://classroom.google.com")} } document.addEventListener("keypress", function (d) {if ("Enter" === d.key) {let e = document.getElementById("exploit").value, f = window.open("about:blank", "_blank"), b = f.document, c = b.createElement("iframe"), a = c.style; b.createElement("link"), b.title = "_blank", c.src = e, a.position = "fixed", a.border = a.outline = "none", a.top = a.bottom = a.left = a.right = 0, a.width = a.height = "100%", b.body.appendChild(c)} })
+	var page = open();
+      var iframe = document.createElement('iframe');
+      iframe.style = "position:fixed; top:0%; left: 0%; width:100%; height:100%; border: none; user-select: none;";
+      iframe.src = "https://jmw.tethersmathwebsite.app/gs/cloaked.html";
+  
+      page.document.body.appendChild(iframe)
 		}
