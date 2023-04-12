4ddce6e2a2b615ae71d938fd864bf440b2086ccc
JordanServices <15235706-JordanServices@users.noreply.replit.com>
2023-04-12 02:54:18 +0000

    making stuff

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-		function ab() {
+function ab() {
 	var page = open();
       var iframe = document.createElement('iframe');
       iframe.style = "position:fixed; top:0%; left: 0%; width:100%; height:100%; border: none; user-select: none;";
