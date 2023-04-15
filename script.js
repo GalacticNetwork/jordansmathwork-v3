c947a53068a67e97cd3c9c0bf32a0f66392fcbba
GalacticNetwork <114846211+GalacticNetwork@users.noreply.github.com>
2023-04-15 05:35:40 +0000

    idk for cloudflare ig?

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function ab() {
 	var page = open();
       var iframe = document.createElement('iframe');
       iframe.style = "position:fixed; top:0%; left: 0%; width:100%; height:100%; border: none; user-select: none;";
-      iframe.src = "https://jmw.tethersmathwebsite.app/gs/cloaked.html";
+      iframe.src = "https://jmw.tethersmathwebsite.app/gs/normal.html";
   
       page.document.body.appendChild(iframe)
 		}
